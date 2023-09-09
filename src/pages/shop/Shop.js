5b344b8666fc15b66ab28987885f0cf957be8dd0
Sontara444 <sontararajput@gmail.com>
2023-09-09 23:13:21 +0530

    setup

--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Shop = () => {
   return (
-    <div></div>
+    <div className="shop">
+      <div className="shopTitle">
+        <h1>My Shop</h1>
+      </div>
+      <div className="products">
+        
+      </div>
+    </div>
   )
 }
---
+++ src/products.js
@@ -0,0 +1,8 @@
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName:"IPhone",
+        price: 999.0,
+        productImage:"src/assets/products/1.png"
+    }
+]
